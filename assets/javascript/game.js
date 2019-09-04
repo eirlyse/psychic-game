7c3c9d8b1262f04ac7b16210bf3924c070e8bcac
Eirlyse <eirlyse.s@gmail.com>
2019-09-04 15:16:57 -0700

    starting javascript, fixing console errors

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,70 @@
 let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
 
+let wins = 0;
+let losses = 0;
+let guessesRemaining = 9;
+let guessesSoFar = [];
+let psychicGuess = [];
 // for (let i=0; i<letters.length, i++) {
 
-let randomLetter = function(){
-    psychic = letters[Math.floor(Math.random() * letters.length)];
-};
+// let randomLetter = function(){
+//    psychic = letters[Math.floor(Math.random() * letters.length)];
+//    console.log(psychicGuess[0]);
+// };
 
-let guessesSoFar = function(){
-    document.getElementById()
+
+document.onkeyup = function (event) {
+    let userGuess = event.key;
+    //guessesSoFar.push(userGuess);
+    console.log(psychicGuess[0]);
 }
 
+if (userGuess === psychicGuess[0]) (guessesRemaining > 0)
+    wins++;
+    guessesRamining = 9;
+    guessesSoFar.length= 0;
+    psychicGuess.length= 0;
+
+    let randomLetter = function(){
+        psychic = letters[Math.floor(Math.random() * letters.length)];
+        console.log(psychicGuess[0]);
+    };
+
+    console.log(computerGuess[0]);
+ 
+    
+
+elseif (userGuess !== psychicGuess[0]) (guessesRemianing > 0)
+    guessesRemaining = guessesRemaining -1;
+    
+
+else {
+
+    losses++;
+    guessesRemaining= 9;
+    guessesSoFar.length=0;
+    psychicGuess.length=0;
+
+    let randomLetter = function(){
+        psychic = letters[Math.floor(Math.random() * letters.length)];
+        console.log(psychicGuess[0]);
+    };
+
+    console.log(psychicGuess[0]);
+
+
+
+}
+
+
+
+// let guesses = function(){
+//   document.getElementById()
+// }
+
+// let guessesRemaining = function(){
+//    document.getElementById()
+// }
 
 
     
