931a294070f2134ef582242e6484ffc6362efaa4
Eirlyse <eirlyse.s@gmail.com>
2019-09-04 14:55:17 -0700

    adding IDs to HTML elements for Javascript to grab

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,15 @@
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+
+// for (let i=0; i<letters.length, i++) {
+
+let randomLetter = function(){
+    psychic = letters[Math.floor(Math.random() * letters.length)];
+};
+
+let guessesSoFar = function(){
+    document.getElementById()
+}
+
+
+
+    
